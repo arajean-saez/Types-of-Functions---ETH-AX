2738ae4388647a741ba6945e997ce517154e6cca
arajean-saez <131237350+arajean-saez@users.noreply.github.com>
2024-05-17 21:06:40 +0800

    Add files via upload

---
+++ AraToken.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract AraToken is ERC20 {
+
+    address public admin;
+    
+    constructor() ERC20("My Ara Token", "MAT") {
+        _mint(msg.sender, 10000 * 10 ** 18);
+        admin = msg.sender;
+    }
+
+    function mint(address to, uint amount) external {
+        require(msg.sender == admin, "only admin");
+        _mint(to, amount);
+    }
+
+    function burn(uint amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        return super.transfer(to, amount);
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
+        return super.transferFrom(from, to, amount);
+    }
+    
+}
